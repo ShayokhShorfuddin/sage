3d7d247f6a04acb576d361578a3f5f1b88054f20
Shayokh <shayokhshuvro14@gmail.com>
2025-06-28 10:06:09 +0600

    add responsiveness to chip

--- components/Chip.tsx
+++ components/Chip.tsx
@@ -2,10 +2,11 @@ import { Sparkles } from "lucide-react";
 
 export default function Chip() {
 	return (
-		<div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-900 rounded-full select-none">
+		<div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-900 rounded-full select-none mx-[1rem]">
 			<Sparkles className="stroke-neutral-200 size-5" />
-			<p className="text-sm text-neutral-200">
-				Trusted By Over 1 Million Developers Worldwide
+			<p className="text-xs xs:text-sm text-neutral-200 text-center">
+				Trusted By Over 1 Million <br className="xs:hidden" /> Developers
+				Worldwide
 			</p>
 		</div>
 	);
