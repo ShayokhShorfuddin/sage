8851d9b2f51cbf109f4bea356df4a4975948bb3a
Shayokh <shayokhshuvro14@gmail.com>
2025-08-18 20:31:13 +0600

    add signup page

---
+++ app/signup/_components/SignUp.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { Eye, EyeOff } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Google from "@/public/images/google.svg";
+import Icon from "@/public/images/icon.png";
+
+export default function Login() {
+	return (
+		<section className="flex flex-col items-center justify-center h-svh">
+			<div className="flex flex-col items-center">
+				<Image src={Icon} alt="icon" className="size-11" />
+
+				<p className="text-2xl text-neutral-300 font-medium mt-1">
+					Create a new account
+				</p>
+
+				<button
+					type="button"
+					className="flex gap-x-3 justify-center items-center text-nowrap w-full bg-neutral-200 text-neutral-800 font-medium mt-6 py-2 rounded-sm hover:cursor-pointer select-none"
+				>
+					<Image src={Google} alt="icon" className="size-5" />
+					<span className="text-sm">Continue with Google</span>
+				</button>
+
+				<div className="flex items-center gap-x-3 w-full mt-2">
+					<div className="h-[0.5px] w-full bg-neutral-800" />
+					<p className="text-neutral-500 text-sm text-nowrap">or use email</p>
+					<div className="h-[0.5px] w-full bg-neutral-800" />
+				</div>
+
+				<form className="flex flex-col w-full gap-y-2 mt-2 text-sm">
+					<input
+						type="text"
+						placeholder="John Doe"
+						className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+					/>
+
+					<input
+						type="email"
+						placeholder="john@example.com"
+						className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500"
+					/>
+
+					<PasswordField />
+
+					<input
+						type="password"
+						placeholder="Confirm your password"
+						className="px-4 py-2 rounded-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
+					/>
+
+					<button
+						type="submit"
+						className="px-3 py-2 hover:cursor-pointer bg-neutral-800 hover:bg-neutral-700 rounded-lg text-sm text-neutral-200 font-medium transition-colors duration-150"
+					>
+						Sign up
+					</button>
+				</form>
+
+				<p className="text-neutral-500 text-sm text-nowrap mt-4">
+					Already have an account?{" "}
+					<Link href="/login" className="text-neutral-300 underline">
+						Login
+					</Link>
+				</p>
+			</div>
+		</section>
+	);
+}
+
+function PasswordField() {
+	const [showPassword, setShowPassword] = useState(false);
+
+	return (
+		<div className="w-full flex">
+			<input
+				type={showPassword ? "text" : "password"}
+				placeholder="Enter your password"
+				className="px-4 py-2 rounded-l-lg border border-neutral-600 bg-neutral-800 text-neutral-200 placeholder-neutral-500 w-full"
+			/>
+
+			<button
+				type="button"
+				className="p-2 border border-l-0 border-neutral-600 bg-neutral-800 rounded-r-lg hover:cursor-pointer"
+				onClick={() => setShowPassword(!showPassword)}
+			>
+				{showPassword ? (
+					<EyeOff size={16} className="text-neutral-300" />
+				) : (
+					<Eye size={16} className="text-neutral-300" />
+				)}
+
+				{showPassword ? (
+					<span className="sr-only">Hide password</span>
+				) : (
+					<span className="sr-only">Show password</span>
+				)}
+			</button>
+		</div>
+	);
+}
