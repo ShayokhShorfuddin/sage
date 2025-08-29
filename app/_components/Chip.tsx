297e01896e53f999f97764781fab0d1c94479148
Shayokh <shayokhshuvro14@gmail.com>
2025-08-29 09:54:49 +0600

    edit the chip text

--- app/_components/Chip.tsx
+++ app/_components/Chip.tsx
@@ -1,13 +1,12 @@
 import { IoHeart } from "react-icons/io5";
 
 export default function Chip() {
-	return (
-		<div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-900 rounded-full select-none mx-[1rem]">
-			<IoHeart/>
-			<p className="text-xs xs:text-sm text-center">
-				Trusted By Over 1 Million <br className="xs:hidden" /> Developers
-				Worldwide
-			</p>
-		</div>
-	);
+  return (
+    <div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-900 rounded-full select-none mx-[1rem]">
+      <IoHeart />
+      <p className="text-xs xs:text-sm text-center">
+        Over 1.3 million interviews conducted.
+      </p>
+    </div>
+  );
 }
